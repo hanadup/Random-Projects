04025b981dc94cce2fd43171d84d5b74b2811fbc
hanadup <92228126+hanadup@users.noreply.github.com>
2022-06-05 22:27:13 +0100

    Update Hangman game.py

--- Hangman game.py
+++ Hangman game.py
@@ -14,12 +14,10 @@ word_length = len(chosen_word)
 end_of_game = False
 lives = 6
 
-#TODO-3: - Import the logo from hangman_art.py and print it at the start of the game.
-
 #Testing code
 print(f'Pssst, the solution is {chosen_word}.')
 
-#Create blanks
+#blanks
 display = []
 for _ in range(word_length):
     display += "_"
@@ -29,7 +27,6 @@ while not end_of_game:
     
     os.system('cls')
 
-    #TODO-4: - If the user has entered a letter they've already guessed, print the letter and let them know.
     if guess in display:
         print("You have already guessed {guess}")
     #Check guessed letter
@@ -48,8 +45,7 @@ while not end_of_game:
         if lives == 0:
             end_of_game = True
             print("You lose.")
-
-    #Join all the elements in the list and turn it into a String.
+            
     print(f"{' '.join(display)}")
 
     #Check if user has got all letters.
@@ -57,5 +53,4 @@ while not end_of_game:
         end_of_game = True
         print("You win.")
 
-    #TODO-2: - Import the stages from hangman_art.py and make this error go away.
     print(pictures.stages[lives])
